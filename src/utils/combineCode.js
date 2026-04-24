c2c3439bdadfca40cdcc27da771949d8e024c2f7
xsabduuh <abdosiifoou@gmail.com>
2026-04-24 21:21:54 +0100

    Update combineCode.js

--- src/utils/combineCode.js
+++ src/utils/combineCode.js
@@ -1,1 +1,16 @@
-
+export function combineCode(html, css, js) {
+  return `
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <style>${css}</style>
+</head>
+<body>
+${html}
+<script>${js}<\/script>
+</body>
+</html>
+  `.trim();
+}
