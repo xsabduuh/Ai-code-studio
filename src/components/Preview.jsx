684558d63f589d3aaf1953e2ddf39b7e59819e41
xsabduuh <abdosiifoou@gmail.com>
2026-04-24 21:20:36 +0100

    Update Preview.jsx

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,1 +1,17 @@
+import React, { useMemo } from "react";
+import { combineCode } from "../utils/combineCode";
 
+const Preview = ({ html, css, js }) => {
+  const sourceDoc = useMemo(() => combineCode(html, css, js), [html, css, js]);
+
+  return (
+    <iframe
+      className="preview-iframe"
+      title="معاينة الكود"
+      sandbox="allow-scripts allow-same-origin"
+      srcDoc={sourceDoc}
+    />
+  );
+};
+
+export default Preview;
